219e005efa2a3951f6416c56e9c72ff1b3bd0f9b
Emmanuel Peter <peterojonilemi@gmail.com>
2022-06-15 10:05:53 +0100

    Solved the employee class problem

--- arr_exer.cpp
+++ arr_exer.cpp
@@ -3,25 +3,57 @@
 #include <string>
 #include <cstring>
 using namespace std;
-void reversit(char a[])
+// void reversit(char a[])
+// {
+//     int i, j;
+//     char temp;
+//     int a_len = strlen(a) - 1;
+//     for (i = a_len, j = 0; i > j; i--, j++)
+//     {
+//         temp = a[i];
+//         a[i] = a[j];
+//         a[j] = temp;
+//     }
+//     cout << "The reversed string is: " << a << endl;
+// }
+
+// int main(void)
+// {
+//     char word[26];
+//     cout << "Enter the string to be reversed: ";
+//     cin.get(word, 26);
+//     reversit(word);
+//     return 0;
+// }
+#define MAX 4
+class Employee
 {
-    int i, j;
-    char temp;
-    int a_len = strlen(a) - 1;
-    for (i = a_len, j = 0; i > j; i--, j++)
+private:
+    string name;
+    long int number;
+
+public:
+    Employee() : name("Emmy"), number(0){};
+    Employee(string n, long int no) : name(n), number(no){};
+    void getdata()
     {
-        temp = a[i];
-        a[i] = a[j];
-        a[j] = temp;
+        cout << "Enter your name: ";
+        cin >> name;
+        cout << "Enter your phone number:";
+        cin >> number;
     }
-    cout << "The reversed string is: " << a << endl;
-}
+    void putdata()
+    {
+        cout << "Your name is " << name << " and your number is " << number << endl;
+    }
+};
 
 int main(void)
 {
-    char word[26];
-    cout << "Enter the string to be reversed: ";
-    cin.get(word, 26);
-    reversit(word);
+    Employee data[MAX];
+    for (int i = 0; i < MAX; i++)
+        data[i].getdata();
+    for (int i = 0; i < MAX; i++)
+        data[i].putdata();
     return 0;
 }
