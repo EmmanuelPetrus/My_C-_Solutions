b4a93e8daea917610565e581c96fefd51cf22ef3
Emmanuel Peter <peterojonilemi@gmail.com>
2022-06-28 13:10:07 +0100

    Corrected some poor coding styles

--- op_over.cpp
+++ op_over.cpp
@@ -677,26 +677,26 @@ using namespace std;
 //     return 0;
 // }
 
-class time12
-{
-private:
-    bool pm;
-    int hrs;
-    int mins;
+// class time12
+// {
+// private:
+//     bool pm;
+//     int hrs;
+//     int mins;
 
-public:
-    time12() : pm(true), hrs(0), mins(0) {}
-    time12(bool ap, int h, int m) : pm(ap), hrs(h), mins(m) {}
-    void display() const
-    {
-        cout << hrs << ':';
-        if (mins < 10)
-            cout << '0';
-        cout << mins << ' ';
-        string am_pm = pm ? "p.m." : "a.m.";
-        cout << am_pm;
-    }
-};
+// public:
+//     time12() : pm(true), hrs(0), mins(0) {}
+//     time12(bool ap, int h, int m) : pm(ap), hrs(h), mins(m) {}
+//     void display() const
+//     {
+//         cout << hrs << ':';
+//         if (mins < 10)
+//             cout << '0';
+//         cout << mins << ' ';
+//         string am_pm = pm ? "p.m." : "a.m.";
+//         cout << am_pm;
+//     }
+// };
 
 // class time24
 // {
@@ -774,101 +774,153 @@ public:
 //     return 0;
 // }
 
-class time24
+// class time24
+// {
+// private:
+//     int hours;
+//     int minutes;
+//     int seconds;
+
+// public:
+//     time24() : hours(0), minutes(0), seconds(0) {}
+//     time24(int h, int m, int s) : hours(h), minutes(m), seconds(s) {}
+//     void display() const // format: 23:15:01
+//     {
+//         if (hours < 10)
+//             cout << '0';
+//         cout << hours << ':';
+//         if (minutes < 10)
+//             cout << '0';
+//         cout << minutes << ':';
+//         if (seconds < 10)
+//             cout << '0';
+//         cout << seconds;
+//     }
+//     int getHrs() const { return hours; }
+//     int getMins() const { return minutes; }
+//     int getSecs() const { return seconds; }
+// };
+
+// class time12
+// {
+// private:
+//     bool pm;
+//     int hrs;
+//     int mins;
+
+// public:
+//     time12() : pm(true), hrs(0), mins(0) {}
+//     time12(bool ap, int h, int m) : pm(ap), hrs(h), mins(m) {}
+//     time12(time24);
+//     void display() const
+//     {
+//         cout << hrs << ':';
+//         if (mins < 10)
+//             cout << '0';
+//         cout << mins << ' ';
+//         string am_pm = pm ? "p.m." : "a.m.";
+//         cout << am_pm;
+//     }
+// };
+
+// time12::time12(time24 t24)
+// {
+//     int hrs24 = t24.getHrs();
+//     pm = t24.getHrs() < 12 ? false : true;
+
+//     mins = (t24.getSecs() < 30) ? t24.getMins() : t24.getMins() + 1;
+//     if (mins == 60)
+//     {
+//         mins = 0;
+//         ++hrs24;
+//         if (hrs24 == 12 || hrs24 == 24)
+//             pm = (pm == true) ? false : true;
+//     }
+//     hrs = (hrs24 < 13) ? hrs24 : hrs24 - 12;
+//     if (hrs == 0)
+//     {
+//         hrs = 12;
+//         pm = false;
+//     }
+// }
+
+// int main(void)
+// {
+//     int h, m, s;
+//     while (true)
+//     {
+//         cout << "Enter 24-hour time: \n";
+//         cout << "    Hours (0 to 23): ";
+//         cin >> h;
+//         if (h > 23)
+//             return (1);
+//         cout << "    Minutes: ";
+//         cin >> m;
+//         cout << "    Seconds: ";
+//         cin >> s;
+
+//         time24 t24(h, m, s);
+//         cout << "You entered: ";
+//         t24.display();
+
+//         time12 t12 = t24;
+
+//         cout << "\n12-hour time: ";
+//         t12.display();
+//         cout << "\n\n";
+//     }
+
+//     return 0;
+// }
+
+// Explicit and mutable keyword
+// class Distance
+// {
+// private:
+//     const float MTF;
+//     int feet;
+//     float inches;
+
+// public:
+//     Distance() : feet(0), inches(0.0), MTF(3.280833F) {}
+//     explicit Distance(float meters) : MTF(3.280833F)
+//     {
+//         float fltfeet = MTF * meters;
+//         feet = int(fltfeet);
+//         inches = 12 * (fltfeet - feet);
+//     }
+//     void showdist()
+//     {
+//         cout << feet << "\'-" << inches << '\"';
+//     }
+// };
+
+// int main(int argc, char const *argv[])
+// {
+//     void fancyDist(Distance);
+//     Distance dist1(2.35F);
+//     return 0;
+// }
+
+class Scrollbar
 {
 private:
-    int hours;
-    int minutes;
-    int seconds;
+    int size;
+    mutable string owner;
 
 public:
-    time24() : hours(0), minutes(0), seconds(0) {}
-    time24(int h, int m, int s) : hours(h), minutes(m), seconds(s) {}
-    void display() const // format: 23:15:01
-    {
-        if (hours < 10)
-            cout << '0';
-        cout << hours << ':';
-        if (minutes < 10)
-            cout << '0';
-        cout << minutes << ':';
-        if (seconds < 10)
-            cout << '0';
-        cout << seconds;
-    }
-    int getHrs() const { return hours; }
-    int getMins() const { return minutes; }
-    int getSecs() const { return seconds; }
+    Scrollbar(int sz, string own) : size(sz), owner(own) {}
+    void setSize(int sz) { size = sz; }
+    void setOwner(string own) const { owner = own; }
+    int getSize() const { return size; }
+    string getOwner() const { return owner; }
 };
-
-class time12
-{
-private:
-    bool pm;
-    int hrs;
-    int mins;
-
-public:
-    time12() : pm(true), hrs(0), mins(0) {}
-    time12(bool ap, int h, int m) : pm(ap), hrs(h), mins(m) {}
-    time12(time24);
-    void display() const
-    {
-        cout << hrs << ':';
-        if (mins < 10)
-            cout << '0';
-        cout << mins << ' ';
-        string am_pm = pm ? "p.m." : "a.m.";
-        cout << am_pm;
-    }
-};
-
-time12::time12(time24 t24)
-{
-    int hrs24 = t24.getHrs();
-    pm = t24.getHrs() < 12 ? false : true;
-
-    mins = (t24.getSecs() < 30) ? t24.getMins() : t24.getMins() + 1;
-    if (mins == 60)
-    {
-        mins = 0;
-        ++hrs24;
-        if (hrs24 == 12 || hrs24 == 24)
-            pm = (pm == true) ? false : true;
-    }
-    hrs = (hrs24 < 13) ? hrs24 : hrs24 - 12;
-    if (hrs == 0)
-    {
-        hrs = 12;
-        pm = false;
-    }
-}
 
 int main(void)
 {
-    int h, m, s;
-    while (true)
-    {
-        cout << "Enter 24-hour time: \n";
-        cout << "    Hours (0 to 23): ";
-        cin >> h;
-        if (h > 23)
-            return (1);
-        cout << "    Minutes: ";
-        cin >> m;
-        cout << "    Seconds: ";
-        cin >> s;
+    const Scrollbar sbar(60, "Window1");
+    sbar.setOwner("Window2");
 
-        time24 t24(h, m, s);
-        cout << "You entered: ";
-        t24.display();
-
-        time12 t12 = t24;
-
-        cout << "\n12-hour time: ";
-        t12.display();
-        cout << "\n\n";
-    }
-
+    cout << sbar.getSize() << ", " << sbar.getOwner() << endl;
     return 0;
 }
