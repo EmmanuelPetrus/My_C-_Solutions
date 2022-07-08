72c1a597d7eeb8b9f658f9871b1f903a404622ee
Emmanuel Peter <peterojonilemi@gmail.com>
2022-07-08 09:38:58 +0100

    Solved the task which invlved the use of pointers to arrange the days of the week alphabetically

--- pointerexer.cpp
+++ pointerexer.cpp
@@ -23,49 +23,84 @@ using namespace std;
 //     return 0;
 // }
 
-class String
-{
-private:
-    char *str;
+// class String
+// {
+// private:
+//     char *str;
 
-public:
-    String(char const *s)
-    {
-        int length = strlen(s);
-        str = new char[length + 1];
-        strcpy(str, s);
-    }
-    char *upit()
-    {
-        int length = strlen(str);
-        for (size_t i = 0; i < length; i++)
-        {
+// public:
+//     String(char const *s)
+//     {
+//         int length = strlen(s);
+//         str = new char[length + 1];
+//         strcpy(str, s);
+//     }
+//     char *upit()
+//     {
+//         int length = strlen(str);
+//         for (size_t i = 0; i < length; i++)
+//         {
 
-            if (*(str + i) == ' ')
-                continue;
-            else
-            {
-                char m = toupper(*(str + i));
-                *(str + i) = m;
-            }
-        }
-        return str;
-    }
-    ~String()
-    {
-        cout << "Deleting str.\n";
-        delete[] str;
-    }
-    void display()
-    {
-        cout << str << endl;
-    }
-};
+//             if (*(str + i) == ' ')
+//                 continue;
+//             else
+//             {
+//                 char m = toupper(*(str + i));
+//                 *(str + i) = m;
+//             }
+//         }
+//         return str;
+//     }
+//     ~String()
+//     {
+//         cout << "Deleting str.\n";
+//         delete[] str;
+//     }
+//     void display()
+//     {
+//         cout << str << endl;
+//     }
+// };
+
+// int main(int argc, char const *argv[])
+// {
+//     String m = "How old are you boy";
+//     char *k = m.upit();
+//     cout << k << endl;
+//     return 0;
+// }
 
 int main(int argc, char const *argv[])
 {
-    String m = "How old are you boy";
-    char *k = m.upit();
-    cout << k << endl;
+    void bsort(const char **p, int n);
+    const int MAX = 7;
+    const char *days[MAX] = {"Sundays", "Mondays", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+    bsort(days, MAX);
+    for (int i = 0; i < MAX; i++)
+    {
+        cout << *(days + i) << endl;
+    }
+
     return 0;
+}
+
+void bsort(const char **pp, int n)
+{
+    void order(const char **, const char **);
+    int j, k;
+    for (j = 0; j < n - 1; j++)
+
+        for (k = j + 1; k < n; k++)
+
+            order(pp + j, pp + k);
+}
+
+void order(const char **pp1, const char **pp2)
+{
+    if ((**pp1) > (**pp2))
+    {
+        const char *tempstr = *pp1;
+        *pp1 = *pp2;
+        *pp2 = tempstr;
+    }
 }
