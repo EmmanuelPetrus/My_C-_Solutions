9baffbf796d569d65d150354966b591d86fbc9c5
Emmanuel Peter <Peterojonilemi@gmail.com>
2022-09-06 09:52:13 +0100

    push of cstring to repo

--- cString.cpp
+++ cString.cpp
@@ -2,8 +2,10 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
+
+
+// int main()
+// {
     // char str1[] = "This is the first string.";
     // char str2[] = "This is the second string.";
     // strcpy(str1, str2);
@@ -47,8 +49,34 @@ int main()
     // *cPtr = 'E';
     // cout << "str after the last change: " << str << endl;
 
-    char str[] = "Hello friends of mine.";
-    char *sPtr = strstr(str, "friends");
-    cout << "The substring starts at index: " << sPtr - str;
-    return 0;
-}
+    // char str[] = "Hello friends of mine.";
+    // char *sPtr = strstr(str, "friends");
+    // cout << "The substring starts at index: " << sPtr - str;
+
+    // char str[] = "Hello friends of mine.";
+    // char *pPtr = strpbrk(str, "pfmd");
+    // cout << "The character " << *pPtr << " was found." << endl;
+    // cout << "It is at index: " << pPtr - str;
+
+    // const char *str1 = "Hello Alice.";
+    // const char *str2 = "Hello John.";
+    // const char *str3 = "Hello Betsy.";
+
+    // cout << "Comparing str1 and str2: ";
+    // cout << strcmp(str1, str2) << endl;
+    // cout << "Comparing str2 and str3: ";
+    // cout << strcmp(str2, str3) << endl;
+
+    // cout <<"Comparing first 5 characters of str1 and str2: ";
+    // cout <<strncmp(str1,str2,5);
+//     char str[] = "July 15,2015";
+//     char *p;
+//     p = strtok(str, " ");
+//     while (p)
+//     {
+//         cout << p << endl;
+//         p = strtok(0, ",");
+//     }
+
+//     return 0;
+// }
