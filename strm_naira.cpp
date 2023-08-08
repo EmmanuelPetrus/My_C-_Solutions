f7405fb076281f6cf1b9133e23aeed70347e2d03
EmmanuelPetrus <peterojonilemi@gmail.com>
2023-08-08 10:47:36 +0100

    added another line of code

--- strm_naira.cpp
+++ strm_naira.cpp
@@ -8,5 +8,12 @@ ostream& currency(ostream& stream)
     stream.precision(2);
     stream.fill('*');
     stream.setf(ios::fixed,ios::floatfield);
-    return
+    return stream;
+}
+
+int main(int argc, char const *argv[])
+{
+    cout << currency <<setw(12)<< 12325.45<<endl;
+    cout << currency << setw(12) << 0.36 << endl;
+    return 0;
 }
